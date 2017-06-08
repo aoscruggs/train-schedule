3be6ff756e924cf2ab5e7d0ab66174318585038f
Afi Scruggs <afiscruggs@gmail.com>
2017-06-08 18:46:17 -0400

    Add files via upload

--- assets/javascript/schedule.js
+++ assets/javascript/schedule.js
@@ -1,3 +1,5 @@
+$( document ).ready(function() {
+
 // Initialize Firebase
   var config = {
     apiKey: "AIzaSyCCSvOu4Mej6TLLSoBIl8rjNAFfdfg9O3c",
@@ -16,50 +18,47 @@
   var destination ="";
   var firstTrainTime = 0;
   var frequency = 0;
+  
 
   //capture button click
   $("#addTrain").on("click", function(event){
-  	event.preventDefault();
-  
-	  // grab input from text boxes
-	  trainName = $("#trainName").val().trim();
-	  destination = $("#destination").val().trim();
-	  firstTrainTime = $("#firstTrainTime").val().trim();
-	  frequency = $("#frequency").val().trim();
-    console.log(trainName);
-    console.log(destination);
-    console.log(firstTrainTime);
-    console.log(frequency);
-  });
+      	event.preventDefault();
+      alert("clicked");
+    	  // grab input from text boxes
+    	  trainName = $("#trainName").val().trim();
+    	  destination = $("#destination").val().trim();
+    	  firstTrainTime = $("#firstTrainTime").val().trim();
+    	  frequency = $("#frequency").val().trim();
+        console.log(trainName);
+        console.log(destination);
+        console.log(firstTrainTime);
+        console.log(frequency);
+
 
 //code for handling the push to Firebase
-		database.ref().on("value", function(childSnapshot){
-      console.log(childSnapshot.val());
+
+      database.ref().push({
+        trainName: trainName, 
+        destination: destination, 
+        firstTrainTime: firstTrainTime,
+        frequency: frequency
+      });
+
+    });
+
+		database.ref().on('child_added', function(childSnapshot){
+      console.log(childSnapshot.val().trainName);
       var newRow = $('<tr>');
-      
-      var newName = $('<td>').text(childSnapshot.val().trainName).appendTo(newRow);
+      alert(childSnapshot.val().trainName);
+      var newName = $('<td>').html(childSnapshot.val().trainName).appendTo(newRow);
       var newRole = $('<td>').text(childSnapshot.val().destination).appendTo(newRow);
       var newStartDate = $('<td>').text(childSnapshot.val().firstTrainTime).appendTo(newRow);
       var newMonthRate = $('<td>').text(childSnapshot.val().frequency).appendTo(newRow);
       
-      newRow.appendTo($('#new-data')); 
+  newRow.appendTo($('#new-data'));
+  
     }); 
 
-/*code for creating the array
-  database.ref().on("value", function(snapshot){
+   });
 
-    //storing snapshot.val() in a variable for later
-    var sv = snapshot.val();
-
-    //for loop to create an array
-    for( var key in sv){
-      var thisObject = sv[key];
-      console.log(thisObject);
-  }
-// getting an array of each key into into Firebase
-    var svArr = Object.keys(sv);
-
- // finding the last user's key
-    var lastIndex = svArr.length - 1;   
-  });
-// code for using the array to populate the schedule table*/
+  
